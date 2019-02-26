340740cae34c1762d911ef16f9957c23cadeb936
brossard <florian.brossard@etu.unistra.fr>
2019-02-26 11:21:54 +0100

    TP Symfony

---
+++ M2/Architecture web/TPSymfony/vendor/src/model/Form.php
@@ -0,0 +1,13 @@
+class Form{
+    private $name;
+    private $lastName;
+    private $street;
+    private $postalCode;
+    private $city;
+    private $phone;
+    private $mail;
+    private $login;
+    private $pass;
+
+    
+}
