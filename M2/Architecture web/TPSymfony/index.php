73229a51fe4e4d5d03ea293506999b8c4f2b4926
brossard <florian.brossard@etu.unistra.fr>
2019-02-26 12:19:39 +0100

    Redirection Slim

--- M2/Architecture web/TPSymfony/index.php
+++ M2/Architecture web/TPSymfony/index.php
@@ -1,10 +1,29 @@
 <?php
     require 'vendor/autoload.php';
 
-    $loader = new Twig_Loader_Filesystem('vendor/src/templates/');
-    $twig = new Twig_Environment($loader, array(
+    $LOADER = new Twig_Loader_Filesystem('src/templates/');
+    $TWIG = new Twig_Environment($LOADER, array(
         'cache' => false
     ));
-    $template = $twig->loadTemplate('form.twig.html');
-    echo $template->render(array('title' => "Formulaire d'inscription"))
+
+    function load_template($template){
+        global $TWIG;
+        return $TWIG->loadTemplate("$template.twig.html");
+    }
+
+    $app = new \Slim\App;
+    $app->redirect('/', '/home', 301);
+    $app->get('/home', function($req, $resp, $args){
+        return $resp->write(load_template('home')->render(array()));
+    });
+    $app->get('/login', function($req, $resp, $args){
+        return $resp->write(load_template('login')->render(array()));
+    });
+    $app->get('/signup', function($req, $resp, $args){
+        return $resp->write(load_template('signup')->render(array()));
+    });
+    $app->post('/signup', function($req, $resp, $args){
+        return "Inscription pas encore implémentée";
+    });
+    $app->run();
 ?>
