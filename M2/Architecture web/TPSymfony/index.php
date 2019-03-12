477b2dd10761705bb939f69b9431de88ed2594c1
brossard <florian.brossard@etu.unistra.fr>
2019-03-12 10:47:23 +0100

    signup OK

--- M2/Architecture web/TPSymfony/index.php
+++ M2/Architecture web/TPSymfony/index.php
@@ -1,6 +1,5 @@
 <?php
     require 'vendor/autoload.php';
-    require 'src/model/User.php';
 
     $LOADER = new Twig_Loader_Filesystem('src/templates/');
     $TWIG = new Twig_Environment($LOADER, array(
@@ -10,21 +9,6 @@
     function load_template($template){
         global $TWIG;
         return $TWIG->loadTemplate("$template.twig.html");
-    }
-
-    function form_to_user($form){
-        error_log(implode(array_keys($form)));
-        $user = new User();
-        $user->name = $form['nom'];
-        $user->lastName = $form['prenom'];
-        $user->street = $form['adresse'];
-        $user->postalCode = $form['code-postale'];
-        $user->city = $form['ville'];
-        $user->phone = $form['telephone'];
-        $user->mail = $form['email'];
-        $user->login = $form['login'];
-        $user->pass = password_hash($form['pass'], PASSWORD_BCRYPT);
-        return $user;
     }
 
     $app = new \Slim\App;
@@ -39,7 +23,8 @@
         return $resp->write(load_template('signup')->render([]));
     });
     $app->post('/signup-validate', function($req, $resp, $args){
-        $user = form_to_user($req->getQueryParams());
+        $user = $req->getParsedBody();
+        $user['pass'] = password_hash($user['pass'], PASSWORD_BCRYPT);
         return $resp->write(load_template('signup-validate')->render([
             'user' => $user
         ]));
